c2587bf5fc0be0b1a07b77568ff8f1338d27cf19
gastonmondin <gaston_mondin@hotmail.com>
2023-12-17 17:17:06 -0300

    Se completó la mission 4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.use(express.static('public_html'));
 
+app.get('/home', (req, res) => res.sendFile(__dirname + '/public_html/index.html'));
 app.get('/ping', (req, res) => res.send('pong'));
 
 app.listen (4000, () => console.log("Servidor corriendo en http://localhost:4000"));
